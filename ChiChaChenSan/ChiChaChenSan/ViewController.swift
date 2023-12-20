89c435c895a4cf656b11d84313c8eef935377ae9
Umair Yousaf <umair.y@theentertainerasia.com>
2023-12-20 23:41:11 +0500

    setup project

---
+++ ChiChaChenSan/ChiChaChenSan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChiChaChenSan
+//
+//  Created by Umair Yousaf on 20/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
